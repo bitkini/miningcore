d3aafe80316e5927d4230a9d20d0cf83d9aab44c
bitkini <team@bitkini.lol>
2025-07-12 20:13:44 +0200

    fix?

--- build-ubuntu-24.04.sh
+++ build-ubuntu-24.04.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
-# Ubuntu 24 includes dotnet-sdk-6.0 out of the box
+# 1) Register Microsoft package repo (for libssl, etc)
+wget -q https://packages.microsoft.com/config/ubuntu/24.04/packages-microsoft-prod.deb \
+     -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+
+# 2) Install native build deps (but not dotnet-sdk via apt)
 sudo apt-get update
 sudo apt-get install -y \
-  dotnet-sdk-6.0 \
+  apt-transport-https \
+  ca-certificates \
   git \
   cmake \
   build-essential \
@@ -21,7 +28,14 @@ sudo apt-get install -y \
   ninja-build \
   clang
 
-# Build MiningCore
+# 3) Install .NET 6 via dotnet-install script
+wget -q https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
+chmod +x dotnet-install.sh
+sudo mkdir -p /usr/share/dotnet
+sudo ./dotnet-install.sh --channel 6.0 --install-dir /usr/share/dotnet
+sudo ln -sf /usr/share/dotnet/dotnet /usr/bin/dotnet
+
+# 4) Build MiningCore
 (
   cd src/Miningcore
   BUILDDIR=${1:-../../build}
